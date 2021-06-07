6fde1767ac0b76af49e9952c0f0ff76ee9138c62
nenadic16 <urosnenadic18@gmail.com>
2021-06-07 15:49:53 +0200

    Dodane forme za sardnika i proizvod

---
+++ HCI-T5.4/HCI-T5.4/TabelaProizvoda.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HCI_T5._4.modeli;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HCI_T5._4
+{
+    /// <summary>
+    /// Interaction logic for TabelaProizvoda.xaml
+    /// </summary>
+    public partial class TabelaProizvoda : Window
+    {
+        public ObservableCollection<Product> Products
+        {
+            get;
+            set;
+        }
+        public TabelaProizvoda()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Products = new ObservableCollection<Product>();
+            Products.Add(new Product {Id="1", Ime = "Petar", Opis = "Petrovic"});
+            Products.Add(new Product { Id = "1", Ime = "Milica", Opis = "Milicevic" });
+            Products.Add(new Product { Id = "1", Ime = "Zoran", Opis = "Zoranovic" });
+            Products.Add(new Product { Id = "1", Ime = "Suzana", Opis = "Suzanic" });
+            Products.Add(new Product { Id = "1", Ime = "Goran", Opis = "Goranski"});
+        }
+    }
+}
---
+++ HCI-T5.4/HCI-T5.4/modeli/Product.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace HCI_T5._4.modeli
+{
+    [Serializable]
+    public class Product : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public string Id { get; set; }
+        private String _ime;
+        private String _opis;
+        [NonSerialized]
+        private WriteableBitmap _slicica;
+
+
+
+        public String Ime
+        {
+            get
+            {
+                return _ime;
+            }
+            set
+            {
+                _ime = value;
+                OnPropertyChanged("Ime");
+            }
+        }
+        public String Opis
+        {
+            get
+            {
+                return _opis;
+            }
+            set
+            {
+                _opis = value;
+                OnPropertyChanged("Opis");
+            }
+        }
+
+
+    }
+}
